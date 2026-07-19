17805bea294d3ebdf86f6be0f45de84390bd97f6
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-19 23:31:55 +0100

    Create Config.tcl

---
+++ internal.w/Config.tcl
@@ -0,0 +1,10 @@
+# Design configuration
+set DESIGN_NAME "cpu"
+set CORES 8
+
+# Library paths (customize for your technology)
+set TECH_TF "../libs/technology_library/tech_file.tf"
+set TLU_NOMINAL "../libs/technology_library/parasitic_model.tluplus"
+
+# Constraints
+set SDC_FILE "./sdc/clocks.sdc"
